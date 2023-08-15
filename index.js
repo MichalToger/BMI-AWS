34a0d47585dfb4cea3c5d54c7341b6d37cc1f519
MichalToger <120334172+MichalToger@users.noreply.github.com>
2023-08-15 20:10:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,84 @@
-const API_BASE_URL = "https://p8j9kem4pj.execute-api.us-east-1.amazonaws.com/production";
-const COGNITO_URL = "https://bmi-login.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=6drqnkmsb5r45492kg3mm034m8&redirect_uri=https://bmi-website-storage.s3-website-us-east-1.amazonaws.com";
+// Cognito Configuration
+const CognitoConfig = {
+    UserPoolId: 'us-east-1_vEHyk3Hwe', // Replace with your User Pool ID
+    ClientId: '1ftbsdm5hfh9edm3i2r5kqon6j', // Replace with your Client ID
+    RedirectUriSignIn: 'http://d39wi8smopyxdy.cloudfront.net/', // CloudFront distribution URL
+    RedirectUriSignOut: 'http://d39wi8smopyxdy.cloudfront.net/', // CloudFront distribution URL
+    UserPoolDomain: 'bmitracker.auth.us-east-1.amazoncognito.com'
+};
 
-function fetchAllRecords() {
-    fetch(API_BASE_URL + '/fetchallrecords')
-    .then(response => response.json())
-    .then(data => {
-        // Process and display the data on your page
-        // For example, populate the 'bmi-table' with the fetched records
-    })
-    .catch(error => {
-        console.error('Error fetching BMI records:', error);
+// API Gateway URL
+const API_URL = 'https://v4s06pp8wg.execute-api.us-east-1.amazonaws.com/1';
+
+$(document).ready(function() {
+    // Add Item
+    $('#addItemForm').submit(function(e) {
+        e.preventDefault();
+        let userName = $('#userName').val();
+        let weight = $('#weight').val();
+        let height = $('#height').val();
+
+        $.ajax({
+            url: API_URL + '/additem',
+            type: 'POST',
+            data: JSON.stringify({
+                'userName': userName,
+                'weight': weight,
+                'height': height
+            }),
+            contentType: 'application/json',
+            success: function(response) {
+                alert('Item added successfully!');
+            },
+            error: function(error) {
+                alert('Failed to add item. ' + error.responseText);
+            }
+        });
     });
-}
 
-function addRecord() {
-    // Add a new BMI record
-    // This will be implemented after user login is set up
-}
+    // Remove Item
+    $('#removeItemForm').submit(function(e) {
+        e.preventDefault();
+        let userName = $('#userNameRemove').val();
 
-function updateRecord(timestamp) {
-    // Update an existing BMI record
-    // This will be implemented after user login is set up
-}
+        $.ajax({
+            url: API_URL + '/removeitem',
+            type: 'DELETE',
+            data: JSON.stringify({
+                'userName': userName
+            }),
+            contentType: 'application/json',
+            success: function(response) {
+                alert('Item removed successfully!');
+            },
+            error: function(error) {
+                alert('Failed to remove item. ' + error.responseText);
+            }
+        });
+    });
 
-function deleteRecord(timestamp) {
-    // Delete a BMI record
-    // This will be implemented after user login is set up
-}
+    // Update Item
+    $('#updateItemForm').submit(function(e) {
+        e.preventDefault();
+        let userName = $('#userNameUpdate').val();
+        let weight = $('#weightUpdate').val();
+        let height = $('#heightUpdate').val();
 
-// Check if the user is logged in (based on the presence of a code in the URL)
-if (window.location.search.includes("code=")) {
-    // The user has logged in via Cognito
-    document.getElementById("bmi-section").style.display = "block";
-    fetchAllRecords();
-} else {
-    // Redirect to Cognito Hosted UI for login/registration
-    window.location.href = COGNITO_URL;
-}
+        $.ajax({
+            url: API_URL + '/updateitem',
+            type: 'PUT',
+            data: JSON.stringify({
+                'userName': userName,
+                'weight': weight,
+                'height': height
+            }),
+            contentType: 'application/json',
+            success: function(response) {
+                alert('Item updated successfully!');
+            },
+            error: function(error) {
+                alert('Failed to update item. ' + error.responseText);
+            }
+        });
+    });
+});
