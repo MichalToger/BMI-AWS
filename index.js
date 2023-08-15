44281ecdb697631e0b71170ef9500f91941ceb1d
MichalToger <120334172+MichalToger@users.noreply.github.com>
2023-08-15 11:52:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const API_BASE_URL = "https://p8j9kem4pj.execute-api.us-east-1.amazonaws.com/production";
 const COGNITO_URL = "https://bmi-login.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=6drqnkmsb5r45492kg3mm034m8&redirect_uri=https://bmi-website-storage.s3-website-us-east-1.amazonaws.com";
 
-function loginWithCognito() {
-    // Redirect to Cognito Hosted UI for login/registration
-    window.location.href = COGNITO_URL;
-}
-
 function fetchAllRecords() {
     // Fetch all BMI records for the logged-in user
     // This will be implemented after user login is set up
@@ -29,7 +24,9 @@ function deleteRecord(timestamp) {
 // Check if the user is logged in (based on the presence of a code in the URL)
 if (window.location.search.includes("code=")) {
     // The user has logged in via Cognito
-    document.getElementById("login-section").style.display = "none";
     document.getElementById("bmi-section").style.display = "block";
     fetchAllRecords();
+} else {
+    // Redirect to Cognito Hosted UI for login/registration
+    window.location.href = COGNITO_URL;
 }
